6adb56d7b2ab202180dfabad35d27b10fafad7da
Thilina19 <92841672+Thilina19@users.noreply.github.com>
2026-02-24 15:48:02 +0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,11 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname === "/") url.pathname = "/index.html";
-    return env.ASSETS.fetch(new Request(url, request));
+    try {
+      const url = new URL(request.url);
+      if (url.pathname === "/") url.pathname = "/index.html";
+      return await env.ASSETS.fetch(new Request(url, request));
+    } catch (e) {
+      return new Response(String(e?.stack || e), { status: 500 });
+    }
   },
 };
